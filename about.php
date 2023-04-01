b87f5509493f965c3c65fdc0c090719c87c154f3
Kendall Woods Step Dad <128986539+prsdntc@users.noreply.github.com>
2023-04-02 00:00:42 +0200

    about file update
    new code to about page

--- about.php
+++ about.php
@@ -32,7 +32,6 @@
             }
             
             .aboutheader a{
-                padding: 12px;
                 text-decoration: none;
             }
             
@@ -40,14 +39,8 @@
                 color: #444;
             }
             
-            
             #header{
                 color: white;
-                font-size: 15px; 
-                font-weight: bold;
-                transition: 0.2s;
-                top: 0;
-                padding-top: 14px;
             }
             
             .headerbanner{}
@@ -82,7 +75,7 @@
         <meta name="description" content="My Eastrand - Connecting local communitiese">
         <meta name="keywords" content="my eastrand, germiston, katlehong, boksburg, kempton park, edenvale, duduza, thokoza, kwa-thema, vosloorus, brakpan, my east rand, my kasi, data, statistics">
         
-        <!-- Fbk ---->
+        <!-- Facebook Share link ---->
         <meta property="og:title" content="My Eastrand - Explore The Eastrand">
         <meta property="og:url" content="http://www.myeastrand.com">
         <meta property="og:image" content="./img/src.png">
@@ -90,31 +83,39 @@
         <meta property="og:description" content="My Eastrand - Connecting local communities">
         
         
+        <!-- Twitter Share link --->
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="About Us - Explore the East Rand" />
+        <meta name="twitter:description" content="The East Rand online portal." />
+        <meta name="twitter:image" content="https://myeastrand.com/img/carnival-city.jpg" />
+        
         <link rel="stylesheet" href="./style/w3.css" type="text/css">
         <link rel="stylesheet" href="./style/style.css" type="text/css">
         <link rel="stylesheet" href="./style/header.css" type="text/css">
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
     </head>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     
     <body style="background-color: white;">
 
 
         
         <!--- header menu --->
-        <header class="w3-top aboutheader"  style="height: 52px;">
+        <header class="w3-container aboutheader">
             <div class="w3-content w3-animate-left" style="max-width: 1200px;">
-                <div class="w3-left w3-round" style="padding: 2px;margin-left: 5px;" onclick="w3_open()">
+                <div class="w3-left w3-padding" style="padding: 2px;margin-left: 5px;" onclick="w3_open()">
                     
                     <img id="logoheader2" class="w3-round" src="./img/My East Rand - Logo.png" alt="Header Navigation Logo">
                 </div>
                 
-                <div class="w3-right " id="header"> <!----->
-                    <a href="home"><i class="fa fa-home"></i> Home</a>
+                <div class="w3-right w3-padding" id="header"> <!----->
+                    <a href="home" class="w3-button w3-round"><i class="fa fa-home"></i> Home</a>
                     <a href="aboutus" class="active-menu"><i class="fa fa-info"></i> About Us</a>
-                    <a href="contact"><i class="fa fa-phone"></i> Contact</a>
-                    <a href="cookie"><i class="fa fa-cookie"></i> Cookie Policy</a>
+                    <a href="contact" class="w3-button w3-round"><i class="fa fa-phone"></i> Contact</a>
+                    <a href="cookie" class="w3-button w3-round"><i class="fa fa-cookie"></i> Cookie Policy</a>
                     
                 </div>
             </div>
@@ -137,56 +138,84 @@
         </div>
         
         <div class="w3-container whatis w3-margin">
-            <div class="w3-center w3-content" style="max-width: 300px;">
+            <div class="w3-content" style="">
                 <h2>What is The East Rand?</h2>
-                <p>The East Rand is one of three Metropolitan Municipalities in Gauteng, South Africa.</p>
+                <p>The East Rand is a major urban area located in the Gauteng province of South Africa. It is the urban eastern part of Witwatersrand that is functionally merged with the Johannesburg conurbation. The region extends from Alberton in the west to Springs in the east, and south down to Nigel. It includes the towns of Bedfordview, Benoni, Boksburg, Brakpan, Edenvale, Germiston, Kempton Park, Linksfield and Modderfontein.<em>(<strong>Source&#58;</strong> <a href="https://en.wikipedia.org/wiki/East_Rand">Wikipedia</a>)</em></p>
+                
+                <div>                    
+                    <!-- Slide Show-->
+                    <div id="carouselExampleCaptions" class="carousel slide w3-mobile" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            
+                            <!-- indicator #1 -->
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            
+                            <!-- indicator #2 -->
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            
+                            <!-- indicator #3 -->
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            
+                            <!-- indicator #4 -->
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        </div>
+                        
+                        <!-- The Slides --->
+                        <div class="carousel-inner">
+                            
+                            <!-- Slide #1 -->
+                            <div class="carousel-item active w3-green">
+                                <img src="./img/carnival-city.jpg" class="d-block w-100" alt="Carnival City">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Carnival City</h5>
+                                    <p>The Fun never fades at Carnival City.</p>
+                                </div>
+                            </div>
+                            
+                            <!-- Slide #2 -->
+                            <div class="carousel-item w3-lime">
+                                <img src="./img/dellvile swimmingpool.jpg" class="d-block w-100" alt="Delville Swimming Pool">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Delville Swimming Pool</h5>
+                                    <p>Germiston, Gauteng.</p>
+                                </div>
+                            </div>
+                            
+                            <!-- Slide #3 -->
+                            <div class="carousel-item w3-orange">
+                                <img src="./img/germiston-city.jpg" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Germiston</h5>
+                                    <p>Germiston lies in the Rand gold fields.</p>
+                                </div>
+                            </div>
+                            
+                            <!-- Slide #4 --->
+                            <div class="carousel-item w3-blue">
+                                <img src="./img/spruitview-germiston-gauteng.jpg" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Spruitview</h5>
+                                    <p>Spyland, K1!</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <!-- Back - Forward Buttons -->
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    <!-- Slide Show-->
+                </div>
+                
             </div>
         </div>
-            <div class="w3-content" style="max-width: 960px;margin-bottom:100px;">
-                
-                <div class="" style="margin-top:40px;margin-bottom:40px;">
-                    
-                    <!-- about -->
-                    <article style="font-style: normal;color: #444;font-size:24px;line-height: 1.1;color: #344854;margin-top: 40px;margin-bottom: 40px;font-weight: 100;">
-                        <div class="w3-row-padding">
-                            <div class="w3-half">
-                                <img class="w3-round" src="img/Johann_Snyman_-_Bedfordview,_Germiston,_South_Africa.jpg" alt="Germiston Bedford View" style="max-width: 450px;width: 100%;">
-                            </div>
-                            <div class="w3-half w3-padding">
-                                <p>The East Rand the urban Eastern region of Gauteng, stretches from Tembisa to Nigel. It includes towns of Katlehong, Vosloorus, Germiston, Boksburg, Brakpan, Kempton Park, Benoni, Springs, Kwa-Thema, Daveyton, Etwatwa &amp; Thokoza.</p>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-                
-                <div class="w3-center w3-margin">
-                    <h3 style="line-height: 1;color: #444;">Newsroom</h3>
-                </div>
-                
-                <!--- Start row -->
-                <div class="w3-row">
-                    
-                        <?php
-                        $conn = $pdo->open();
-                        $stmt = $conn->prepare("SELECT * FROM newsroom ORDER BY post_date LIMIT 2");
-                        $stmt->execute();
-                        
-                        foreach($stmt as $row){
-                            echo"<div class='w3-half w3-mobile'>
-                                 <div class='w3-container w3-margin'>
-                                 
-                                 <img class='w3-card w3-round' style='max-width:370px;width:100%;height: 250px;object-fit: cover;' alt=".$row['title']." src='./newspostimg/".$row['image']."'/>
-                                
-                                 <h3 style='color:#666;line-height: 0.9;' class='w3-hover-opacity><a style='text-decoration: none;color:#344854;' href='newsroom.php?read=".$row['link_add']."'>".$row['title']."</a></h3>
-                                 
-                                 <span class='w3-center' style='color:#344854;font-weight: 500;line-height:1.5;'>".date('M d, Y', strtotime($row['post_date']))."</span>
-                                 <div class='w3-opacity'  style='line-height:1.1; font-style: italic;'>".$row['sub_title']."</div>
-                                 </div>
-                                 </div>";
-                        }
-                        ?>
-                        </div>
-                </div>
                 <!--// End row -->
 
 
